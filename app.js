9ae1ca1c748fd44ba38e9143bd520c25e2cdcf48
AbhiDroid <abhinabroy2001@gmail.com>
2023-05-16 13:00:24 +0530

    I did it finally 🤗

--- app.js
+++ app.js
@@ -13,77 +13,10 @@ const transporter = nodemailer.createTransport({
     }
 });
 
-
-
-
-// const task = cron.schedule('5 * * * *', () => {
-//     scrapper();
-// });
-// // const task = cron.schedule('0 9 * * *', () => {
-// //     scrapper();
-// // });
-
-// task.start();
-
-
-
-// const puppeteer = require("puppeteer");
-// require("dotenv").config();
-// const cron = require('node-cron');
-
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-    // const scrapeLogic = async () => {
-    //     const browser = await puppeteer.launch({
-    //       args: [
-    //         "--disable-setuid-sandbox",
-    //         "--no-sandbox",
-    //         "--single-process",
-    //         "--no-zygote",
-    //       ],
-    //       executablePath:
-    //         process.env.NODE_ENV === "production"
-    //           ? process.env.PUPPETEER_EXECUTABLE_PATH
-    //           : puppeteer.executablePath(),
-    //     });
-    //     try {
-    //       const page = await browser.newPage();
-
-    //       await page.goto("https://developer.chrome.com/");
-
-    //       // Set screen size
-    //       await page.setViewport({ width: 1080, height: 1024 });
-
-    //       // Type into search box
-    //       await page.type(".search-box__input", "automate beyond recorder");
-
-    //       // Wait and click on first result
-    //       const searchResultSelector = ".search-box__link";
-    //       await page.waitForSelector(searchResultSelector);
-    //       await page.click(searchResultSelector);
-
-    //       // Locate the full title with a unique string
-    //       const textSelector = await page.waitForSelector(
-    //         "text/Customize and automate"
-    //       );
-    //       const fullTitle = await textSelector.evaluate((el) => el.textContent);
-
-    //       // Print the full title
-    //       const logStatement = `The title of this blog post is ${fullTitle}`;
-    //       console.log(logStatement);
-    //       res.send(logStatement);
-
-    //     } catch (e) {
-    //       console.error(e);
-    //       res.send("Semething went wrong -> ", e);
-
-    //     } finally {
-    //       await browser.close();
-    //     }
-    //   };
 
     const scrapper =  async () => {
         //PAST 24 HOURS
@@ -194,7 +127,7 @@ app.get("/", (req, res) => {
 
     };
 
-    const task = cron.schedule('* * * * *', () => {
+    const task = cron.schedule('0 9 * * *', () => {
         scrapper();
     });
 
